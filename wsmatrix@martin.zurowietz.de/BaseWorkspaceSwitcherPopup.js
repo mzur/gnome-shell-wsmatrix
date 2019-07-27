28138946c7e3c131718ca94508631dc8918b409c
Martin Zurowietz <martin@zurowietz.de>
2019-07-27 14:45:22 +0200

    Implement draft of simpler version of the workspace oveview
    References #13

--- wsmatrix@martin.zurowietz.de/BaseWorkspaceSwitcherPopup.js
+++ wsmatrix@martin.zurowietz.de/BaseWorkspaceSwitcherPopup.js
@@ -12,8 +12,14 @@ class BaseWorkspaceSwitcherPopup extends WorkspaceSwitcherPopup {
    display(direction, activeWorkspaceIndex) {
       super.display(direction, activeWorkspaceIndex);
 
-      Mainloop.source_remove(this._timeoutId);
-      this._timeoutId = Mainloop.timeout_add(this._popupTimeout, this._onTimeout.bind(this));
-      GLib.Source.set_name_by_id(this._timeoutId, '[gnome-shell] this._onTimeout');
+      if (this._timeoutId !== 0) {
+         Mainloop.source_remove(this._timeoutId);
+         this._timeoutId = 0;
+      }
+
+      if (this._popupTimeout > 0) {
+         this._timeoutId = Mainloop.timeout_add(this._popupTimeout, this._onTimeout.bind(this));
+         GLib.Source.set_name_by_id(this._timeoutId, '[gnome-shell] this._onTimeout');
+      }
    }
 });
--- wsmatrix@martin.zurowietz.de/extension.js
+++ wsmatrix@martin.zurowietz.de/extension.js
@@ -3,18 +3,15 @@ const Lang = imports.lang;
 const Meta = imports.gi.Meta;
 const Settings = WsMatrix.imports.Settings.Settings;
 const WmOverride = WsMatrix.imports.WmOverride.WmOverride;
-const WorkspaceOverview = WsMatrix.imports.WorkspaceOverview.WorkspaceOverview;
 
 class WsMatrixExtension {
    constructor() {
       let settings = new Settings(WsMatrix.metadata['settings-schema']);
       let keybindings = new Settings(WsMatrix.metadata['keybindings-schema']);
-      this.override = new WmOverride(settings);
-      this.overview = new WorkspaceOverview(keybindings);
+      this.override = new WmOverride(settings, keybindings);
    }
    destroy() {
       this.override.destroy();
-      this.overview.destroy();
    }
 }
 
