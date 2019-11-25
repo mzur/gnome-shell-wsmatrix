cbe61a969330b04781bdbd2888038479ba2114af
Ptits de Barbe <ettavolt@gmail.com>
2019-11-25 21:05:42 +0300

    Compatibility with gjs 1.58.2.
    Wrap remaining ES6 classes in GObject.registerClass(). Override Gio.Settings._init instead of constructor.

--- wsmatrix@martin.zurowietz.de/WsmatrixThumbnail.js
+++ wsmatrix@martin.zurowietz.de/WsmatrixThumbnail.js
@@ -1,11 +1,13 @@
+const GObject = imports.gi.GObject;
 const Main = imports.ui.main;
 const WorkspaceThumbnail = imports.ui.workspaceThumbnail.WorkspaceThumbnail;
 
-var WsmatrixThumbnail = class WsmatrixThumbnail extends WorkspaceThumbnail {
+var WsmatrixThumbnail = GObject.registerClass(
+class WsmatrixThumbnail extends WorkspaceThumbnail {
     constructor(metaWorkspace, monitorIndex) {
         let tempPrimaryIndex = Main.layoutManager.primaryIndex;
         Main.layoutManager.primaryIndex = monitorIndex;
         super(metaWorkspace);
         Main.layoutManager.primaryIndex = tempPrimaryIndex;
     }
-}
+});
