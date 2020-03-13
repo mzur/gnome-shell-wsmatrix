9e0db5c96283e060d7bd798696c23e722d5ca398
Martin Zurowietz <martin@zurowietz.de>
2020-03-13 14:35:12 +0100

    Update BaseWorkspaceSwitcherPopup for GNOME 3.36

--- wsmatrix@martin.zurowietz.de/BaseWorkspaceSwitcherPopup.js
+++ wsmatrix@martin.zurowietz.de/BaseWorkspaceSwitcherPopup.js
@@ -1,6 +1,5 @@
 const { GLib, GObject} = imports.gi;
 const WorkspaceSwitcherPopup = imports.ui.workspaceSwitcherPopup.WorkspaceSwitcherPopup;
-const Mainloop = imports.mainloop;
 
 var BaseWorkspaceSwitcherPopup = GObject.registerClass(
 class BaseWorkspaceSwitcherPopup extends WorkspaceSwitcherPopup {
@@ -13,12 +12,12 @@ class BaseWorkspaceSwitcherPopup extends WorkspaceSwitcherPopup {
       super.display(direction, activeWorkspaceIndex);
 
       if (this._timeoutId !== 0) {
-         Mainloop.source_remove(this._timeoutId);
+         GLib.source_remove(this._timeoutId);
          this._timeoutId = 0;
       }
 
       if (this._popupTimeout > 0) {
-         this._timeoutId = Mainloop.timeout_add(this._popupTimeout, this._onTimeout.bind(this));
+         this._timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, this._popupTimeout, this._onTimeout.bind(this));
          GLib.Source.set_name_by_id(this._timeoutId, '[gnome-shell] this._onTimeout');
       }
    }
