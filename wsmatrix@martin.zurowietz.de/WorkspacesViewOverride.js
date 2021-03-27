50bbbc99e91fe93a9b27e055d4a45097287b050c
Martin Zurowietz <martin@zurowietz.de>
2021-03-27 14:33:41 +0100

    Fix window animation direction in overview

--- wsmatrix@martin.zurowietz.de/WorkspacesViewOverride.js
+++ wsmatrix@martin.zurowietz.de/WorkspacesViewOverride.js
@@ -1,4 +1,4 @@
-const { Clutter } = imports.gi;
+const { Clutter, Meta } = imports.gi;
 const workspacesView = imports.ui.workspacesView;
 const WorkspacesView = workspacesView.WorkspacesView;
 
@@ -39,15 +39,18 @@ var WorkspacesViewOverride = class {
 
    overrideOriginalProperties() {
       WorkspacesView.prototype._overrideProperties = {
-         _updateWorkspaceActors: WorkspacesView.prototype._updateWorkspaceActors,
+         _updateScrollPosition: WorkspacesView.prototype._updateScrollPosition,
+         _activeWorkspaceChanged: WorkspacesView.prototype._activeWorkspaceChanged,
       };
-      WorkspacesView.prototype._updateWorkspaceActors = this._updateWorkspaceActors;
+      WorkspacesView.prototype._updateScrollPosition = this._updateScrollPosition;
+      WorkspacesView.prototype._activeWorkspaceChanged = this._activeWorkspaceChanged;
       WorkspacesView.prototype.getRows = this.getRows.bind(this);
       WorkspacesView.prototype.getColumns = this.getColumns.bind(this);
    }
 
    restoreOriginalProperties() {
-      WorkspacesView.prototype._updateWorkspaceActors = WorkspacesView.prototype._overrideProperties._updateWorkspaceActors;
+      WorkspacesView.prototype._updateScrollPosition = WorkspacesView.prototype._overrideProperties._updateScrollPosition;
+      WorkspacesView.prototype._activeWorkspaceChanged = WorkspacesView.prototype._overrideProperties._activeWorkspaceChanged;
       delete WorkspacesView.prototype._overrideProperties;
       delete WorkspacesView.prototype.getRows;
       delete WorkspacesView.prototype.getColumns;
@@ -61,52 +64,35 @@ var WorkspacesViewOverride = class {
       return this.columns;
    }
 
-   // Update workspace actors parameters
-   // @showAnimation: iff %true, transition between states
-   _updateWorkspaceActors(showAnimation) {
-      let workspaceManager = global.workspace_manager;
-      let active = workspaceManager.get_active_workspace_index();
-      let activeRow = Math.floor(active / this.getColumns());
-      let activeColumn = active % this.getColumns();
-      this._animating = showAnimation;
+   _activeWorkspaceChanged(_wm, _from, _to, _direction) {
+      if (this._scrolling)
+         return;
 
-      for (let w = 0; w < this._workspaces.length; w++) {
-         let workspace = this._workspaces[w];
-         let workspaceRow = Math.floor(w / this.getColumns());
-         let workspaceColumn = w % this.getColumns();
+      this._wsmatrixDirection = _direction;
 
-         workspace.remove_all_transitions();
+      this._scrollToActive();
+    }
 
-         let params = {};
-         if (this.text_direction == Clutter.TextDirection.RTL) {
-            params.x = (activeColumn - workspaceColumn) * this._fullGeometry.width;
-         } else {
-            params.x = (workspaceColumn - activeColumn) * this._fullGeometry.width;
-         }
-         params.y = (workspaceRow - activeRow) * this._fullGeometry.height;
+   _updateScrollPosition() {
+      if (!this.has_allocation())
+         return;
 
-         if (showAnimation) {
-            let easeParams = Object.assign(params, {
-               duration: workspacesView.WORKSPACE_SWITCH_TIME,
-               mode: Clutter.AnimationMode.EASE_OUT_CUBIC,
-            });
-            // we have to call _updateVisibility() once before the
-            // animation and once afterwards - it does not really
-            // matter which tween we use, so we pick the first one ...
-            if (w == 0) {
-               this._updateVisibility();
-               easeParams.onComplete = () => {
-                  this._animating = false;
-                  this._updateVisibility();
-               };
-            }
-            workspace.ease(easeParams);
-         } else {
-            workspace.set(params);
-            if (w == 0) {
-               this._updateVisibility();
-            }
-         }
+      const adj = this._scrollAdjustment;
+
+      if (adj.upper == 1)
+         return;
+
+      const workspaceManager = global.workspace_manager;
+      const vertical = this._wsmatrixDirection === Meta.MotionDirection.DOWN || this._wsmatrixDirection === Meta.MotionDirection.UP;
+      const rtl = this.text_direction === Clutter.TextDirection.RTL;
+      const progress = vertical || !rtl
+         ? adj.value : adj.upper - adj.value - 1;
+
+      for (const ws of this._workspaces) {
+         if (vertical)
+            ws.translation_y = -progress * this.height;
+         else
+            ws.translation_x = -progress * this.width;
       }
    }
 }
