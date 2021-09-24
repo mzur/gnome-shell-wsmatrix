e185ec930ac7397e982d5b3f4fdaba0fadadf010
Almarhoon Ibraheem <ibeem@outlook.com>
2021-09-24 12:29:41 +0300

    remove unused function _computeWorkspacesBoxForState in thumbnailsBox

--- wsmatrix@martin.zurowietz.de/overview/thumbnailsBox.js
+++ wsmatrix@martin.zurowietz.de/overview/thumbnailsBox.js
@@ -288,40 +288,6 @@ var ThumbnailsBox = class {
                 childBox.y2 = indicatorY2 + indicatorBottomFullBorder;
                 this._indicator.allocate(childBox);
             },
-
-            _computeWorkspacesBoxForState(state, workAreaBox, searchHeight, dashHeight, thumbnailsHeight) {
-                const workspaceBox = workAreaBox.copy();
-                const [startX, startY] = workAreaBox.get_origin();
-                const [width, height] = workspaceBox.get_size();
-                const {spacing} = this;
-                const {expandFraction} = this._workspacesThumbnails;
-                let workspaceManager = global.workspace_manager;
-                let rows = workspaceManager.layout_rows;
-
-                switch (state) {
-                    case ControlsState.HIDDEN:
-                        break;
-                    case ControlsState.WINDOW_PICKER:
-                        workspaceBox.set_origin(startX,
-                            startY + searchHeight + spacing +
-                            thumbnailsHeight * rows + spacing * expandFraction);
-                        workspaceBox.set_size(width,
-                            height -
-                            dashHeight - spacing -
-                            searchHeight - spacing -
-                            thumbnailsHeight * rows - spacing * expandFraction);
-                        break;
-                    case ControlsState.APP_GRID:
-                        workspaceBox.set_origin(startX, startY + searchHeight + spacing+
-                            thumbnailsHeight * rows + spacing * expandFraction);
-                        workspaceBox.set_size(
-                            width,
-                            Math.round(height * rows * SMALL_WORKSPACE_RATIO));
-                        break;
-                }
-
-                return workspaceBox;
-            },
         }
     }
 
